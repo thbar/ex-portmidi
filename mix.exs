5626f0aefc02d61945699d7fbf6e6c758d2235b1
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-04-28 13:56:09 +0200

    Upgrade all dependencies (#3)
    * Bump up dependencies
    
    * Bump up credo
    
    * Bump up earmark

--- mix.exs
+++ mix.exs
@@ -30,9 +30,9 @@ defmodule PortMidi.Mixfile do
 
   defp deps do
     [
-      {:credo, "~> 0.5.3", only: [:dev, :test]},
-      {:mock, "~> 0.1.1", only: :test},
-      {:ex_doc, github: "elixir-lang/ex_doc", only: :dev},
+      {:credo, "~> 1.5.5", only: [:dev, :test]},
+      {:mock, "~> 0.3.6", only: :test},
+      {:ex_doc, "~> 0.24.2", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev}
     ]
   end
